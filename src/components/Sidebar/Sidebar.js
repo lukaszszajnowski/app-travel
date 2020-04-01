d6b718ce8156e146797b3551647038f9ef680ccc
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-04-01 17:23:05 +0200

    Add dots to the sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,11 +5,16 @@ import sidebar_data from '../../utils/sidebar_data';
 const recommendedHotels = sidebar_data.map((item, index ) => {
 return (
 <div key="index" className="sidebar__element">
+    <div className="left-div">
     <img src={item.image}/>
-    <div>
+    </div>
+    <div className="center-div">
     <h2>{item.title}</h2>
     <h3>{item.location}</h3>
     <div class="badge badge-danger">{item.price + "$"}</div>
+    </div>
+    <div className="right-div">
+    <i class="fas fa-ellipsis-h"></i>
     </div>
 </div>
 )
