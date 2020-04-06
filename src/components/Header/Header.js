fa47b84772622c404fb2296660472e5df9e90584
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-04-06 20:16:27 +0200

    Add price field

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,15 @@ class Header extends React.Component {
 
   render() {
     return (
+      <>
       <div className="header">
         <div className="search-loupe-circle"><i class="fas fa-search search-loupe"></i></div>
         <input type="text" className="search-field" placeholder="Enter location" onChange={this.handleSearch} />
       </div>
+      <div className="header-second">
+        <div className="price-field"><i class="fas fa-money-bill-wave dollar-icon"></i><input type="text" className="pice-filter" placeholder="Min. Price"/><i class="fas fa-dollar-sign dollar-icon"></i></div>
+      </div>
+      </>
     );
   }
 }
