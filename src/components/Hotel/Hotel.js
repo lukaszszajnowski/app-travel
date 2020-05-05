232b5d8b384742b8accda2d8ccb51ae4f32711e7
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-05-05 10:43:19 +0200

    Fix currency symbol

--- src/components/Hotel/Hotel.js
+++ src/components/Hotel/Hotel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Hotel.scss';
 
-const Hotel = ({ data }) => {
+const Hotel = ({ data, currencySymbol }) => {
 return (
 <div className="hotel" key={data.id}>
                 <img src={data.image} alt={data.title} />
@@ -11,7 +11,7 @@ return (
                     <span className="hotel-name">{data.title}</span>
                   </Link>
                   <p>{data.location}</p>
-                  {/* <span className="hotel-price">{data.price}{props.currencySymbol}</span> */}
+                  <span className="hotel-price">{data.price}{currencySymbol}</span>
                 </div>
               </div>
               )
