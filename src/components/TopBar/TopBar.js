3239b6062306c74220a985ff59afb49c81f62b74
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-05-06 12:49:15 +0200

    Make the TopBar responsive

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -20,7 +20,7 @@ class TopBar extends React.Component {
         return ( 
             <nav className="top-bar-navi">
                 <ul>
-                <Link to='/'><img src={logo} alt='logo' /></Link>
+                <Link to='/'><img src={logo} className="logo" alt='logo' /></Link>
                 <li><Link to='/'><i className="fas fa-home"></i> Home</Link></li>
                 <li><Link to='/contact-us'><i className="fas fa-comments"></i> Contact us</Link></li>
 
@@ -38,8 +38,8 @@ class TopBar extends React.Component {
                     <>
                     <li><Link to="/add-hotel"><i className="fas fa-plus-circle"></i> Add hotel</Link></li>
                     <li><Link to="/my-hotels"><i class="fas fa-building"></i> My hotels</Link></li>
+                    <div className="user-profile"><i className="fas fa-user"></i> {user.username}</div>
                     <input type="button" value="Logout" className="btn btn-outline-primary" onClick={this.handleLogoutClick} />
-                    <div className="user-profile"><i className="fas fa-user"></i> user: {user.username}</div>
                     </>
                     )
                 }
