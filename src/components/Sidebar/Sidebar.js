ac80e194571e348e94ff0d5c36f7cba36540d7b4
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-05-21 11:51:35 +0200

    Redux implementation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Sidebar.scss';
 import AddHotel from '../AddHotel/AddHotel';
-import Preloader from '../Preloader/Preloader';
-import axios from 'axios';
+// import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Sidebar extends React.Component {
 
   state = {
-    sidebarData: [],
-    isLoading: true,
+    // sidebarData: [],
+    // hotels: [],
     show: false
   };
 
@@ -18,27 +18,24 @@ class Sidebar extends React.Component {
     });
   };
 
-  componentDidMount() {
-    const API = 'https://nodejs-mysql-it-academy.herokuapp.com/hotels/recommended';
-    axios.get(API).then((res) => { 
-    this.setState({
-      sidebarData: res.data,
-      isLoading: false
-    })
-  });
-  }
+  // componentDidMount() {
+  //   const API = 'https://nodejs-mysql-it-academy.herokuapp.com/hotels/recommended';
+  //   axios.get(API).then((res) => { 
+  //   this.setState({
+  //     sidebarData: res.data,
+  //     isLoading: false
+  //   })
+  // });
+  // }
 
   render() {
     return (
       <div className="sidebar">
         <div className="sidebar-block sidebar-hotels">
-        {this.state.isLoading ? 
-              <Preloader />
-                  : 
-              <h2>More than just hotels</h2>}
+              <h2>More than just hotels</h2>
           <div className="s-list">
-            {this.state.sidebarData.map((hotel, index) => {
-              return (
+            {this.props.hotels.map((hotel, index) => {
+              if (hotel.recommended) return (
                 <div className="s-hotel" key={index}>
                   <div
                     className="s-photo"
@@ -65,4 +62,10 @@ class Sidebar extends React.Component {
   }
 }
 
-export default Sidebar;
+const mapStateToProps = (state) => ({
+  hotels: state.hotels,
+});
+
+
+export default connect(mapStateToProps)(Sidebar);
+// export default Sidebar;
